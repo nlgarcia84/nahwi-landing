6d20cfe4d5bad25f21fb74ad885fd907b9097d12
Norman Leyva <nleyvagarcia@gmail.com>
2024-09-24 01:58:40 +0200

    social media icons added

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -7,11 +7,39 @@ const FooterBox = styled.div`
   text-align: center;
 `;
 
+const SocialMediaContainer = styled.div`
+  display: flex;
+  justify-content: space-around;
+  flex-direction: row;
+  width: 50%;
+  margin-left: auto;
+  margin-right: auto;
+`;
+
+const Img = styled.img`
+  width: 1.5em;
+`;
+
+const Rights = styled.p`
+  padding-bottom: 1em;
+`;
+
 export const Footer = () => {
   return (
     <>
       <FooterBox>
-        <p>2024 - Desarrollado por Desertdrop ©</p>
+        <Rights>2024 - Desarrollado por Desertdrop ©</Rights>
+        <SocialMediaContainer>
+          <a href="https://www.instagram.com/nahwi.pro/" target="_blank">
+            <Img src="src/assets/images/ig.webp" />
+          </a>
+          <a href="https://www.tiktok.com/@nahwi.pro" target="_blank">
+            <Img src="src/assets/images/tiktok.webp" />
+          </a>
+          <a href="https://www.x.com/nahwipro/" target="_blank">
+            <Img src="src/assets/images/x.webp" />
+          </a>
+        </SocialMediaContainer>
       </FooterBox>
     </>
   );
